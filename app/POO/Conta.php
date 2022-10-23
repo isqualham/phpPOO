2ebb545a8335faf91b1a391de526c72cb0508c1c
william de freitas <williamfacme@gmail.com>
2022-10-23 20:45:25 -0300

    chore atualziar 50

--- app/POO/Conta.php
+++ app/POO/Conta.php
@@ -14,26 +14,21 @@ class Conta
                 "saldo" => $saldo
             ]
         ];
-    }
-
-    public function getConta()
-    {
-        return $this->conta;
-    }
+    }    
 
     public function operacaoSacar(string $cpf, float $valor):array
     {
         if ($this->conta[$cpf]['saldo'] < $valor)            
             return [
                 'menssage'=>'saldo insuficiente',
-                $this->getConta()
+                $this->conta
             ];
 
         $this->conta[$cpf]['saldo'] -= $valor;
 
         return [
             'menssage'=>'saque realizado com sucesso',
-            $this->getConta()
+            $this->conta
         ];
     }
 
@@ -42,14 +37,14 @@ class Conta
         if($valor < 0)
             return [
                 'menssage'=>'depositos precizam ser positivos',
-                'extrado' =>$this->getConta()
+                'extrado' =>$this->conta
             ];
 
         $this->conta[$cpf]['saldo'] += $valor;
 
         return [
             'menssage'=>'deposito realizado com sucesso',
-            'extrado' =>$this->getConta()
+            'extrado' =>$this->conta
         ];
     }
 }
