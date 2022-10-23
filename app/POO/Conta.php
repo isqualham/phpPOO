d9dce63c58f15f4937c43355873502f570ae7e95
william de freitas <williamfacme@gmail.com>
2022-10-23 17:31:50 -0300

    chore atuali

--- app/POO/Conta.php
+++ app/POO/Conta.php
@@ -4,7 +4,6 @@ namespace App\POO;
 
 class Conta
 {
-
     private $conta = [];
 
     public function __construct($cpf, $name, $saldo)
@@ -17,28 +16,25 @@ class Conta
         ];
     }
 
-
     public function getConta()
     {
         return $this->conta;
     }
 
-    
-    public function setSacarConta($cpf, $valor)
-    {       
-
+    public function setSaldo($cpf, $valor)
+    {
         $this->conta[$cpf]['saldo'] -= $valor;
     }
 
-    public function operacaoSacar($cpf, $valor){
+    public function operacaoSacar($cpf, $valor)
+    {
+        if ($this->conta[$cpf]['saldo'] < $valor)
+            return 'saldo insuficiente, seu saldo atula é: R$ ' .
+                    $this->conta[$cpf]['saldo'];
 
-        if( $this->conta[$cpf]['saldo'] < $valor)
-            return 'saldo insuficiente, seu saldo atula é: R$ '.
-            $this->conta[$cpf]['saldo'];
-        
-        $this->setSacarConta($cpf, $valor);
+        $this->setSaldo($cpf, $valor);
 
-        return 'saque com sucesso seu saldo é: R$ '. $this->conta[$cpf]['saldo'];
+        return 'saque com sucesso seu saldo é: R$ ' .
+                $this->conta[$cpf]['saldo'];
     }
-    
 }
