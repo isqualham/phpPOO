aed56c42a0a638c74bbd1063bab5382918c5bb1c
william de freitas <williamfacme@gmail.com>
2022-11-22 21:30:31 -0300

    chore atualizar 3

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,23 +6,24 @@ use App\Jobs\test;
 use App\POO\Conta;
 use App\POO\ContaCorrente;
 use App\POO\Titular;
+use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-
+    
     public function index(){
         
-        $conta1 = new Conta(new Titular("023.454.911-40","william"));
-        $conta2 = new Conta(new Titular("023.454.911-40","william"));
+        // $conta1 = new Conta(new Titular("023.454.911-40","william"));
+        // $conta2 = new Conta(new Titular("023.454.911-40","william"));
 
-        $conta1->depositar(1000);
-        $conta1->transferir(-1, $conta2);
+        // $conta1->depositar(1000);
+        // $conta1->transferir(-1, $conta2);
 
-        return [
-            "saldo 1: {$conta1->getSaldo()} - saldo 2: {$conta2->getSaldo()}",
-            "titular conta 1 : {$conta1->getTitularName()->getName()}"
-        ];
+        // return [
+        //     "saldo 1: {$conta1->getSaldo()} - saldo 2: {$conta2->getSaldo()}",
+        //     "titular conta 1 : {$conta1->getTitularName()->getName()}"
+        // ];
 
-        
+               
     }
 }
